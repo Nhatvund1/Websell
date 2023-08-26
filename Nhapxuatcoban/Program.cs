209d431f5edefda6d6d63b87dba0be898cca2fcb
Nhatvund1 <nhatvu.tainang107@gmail.com>
2023-08-26 18:25:32 +0700

    init project

---
+++ Nhapxuatcoban/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nhapxuatcoban
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            /*   Console.Write();
+               Console.WriteLine();
+               Console.Read();
+               Console.ReadLine();
+               Console.ReadKey();
+               */
+            
+            Console.Write("K team"); // In ra màn hình dòng chữ K Team 
+            Console.Write(10); // in ra màn hình số 10
+            Console.Write(10.9f); // In ra màn hình số 10.9 (f biểu thị cho kiểu dữ liệu float sẽ được trình bày chi tiết ở bài 5)
+            Console.Write(true);// In ra màn hình chữ true của kiểu dữ liệu bool(sẽ được trình bày chi tiết ở bài 5)
+
+            
+        }
+    }
+}
